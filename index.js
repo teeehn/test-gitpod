78fdcef4dd1e5f44186c3e39fb62d0f4c4a2cde5
Tom Nicolosi <tom@teeehn.com>
2020-05-08 21:42:47 +0000

    Added a form input and state via reducer. Works!

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
 
+function TextInput({
+    id,
+    label,
+    placeholder,
+    value,
+    handleChange
+}) {
+    return (
+        <div className="form-group">
+            <label htmlFor={id} className="form-label">{label}</label>
+            <input
+                id={id}
+                onChange={(e) => handleChange(id, e)}
+                placeholder={placeholder || ""}
+                type="text"
+                value={value}
+            />
+        </div>
+    )
+}
+
 function App() {
+
+    const initial_state = {
+        form_values: {
+            "test-input": ""
+        }
+    }
+
+    function reducer(state, action) {
+        switch(action.type) {
+            case "FORM_UPDATE": return {
+                ...state,
+                form_values: {
+                    ...state.form_values,
+                    [action.payload.id]: action.payload.value
+                }
+            }
+            default: 
+                return state;
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, initial_state);
+
+    function handleChange(id, e) {
+        dispatch({
+            type: "FORM_UPDATE",
+            payload: {
+                id,
+                value: e.target.value
+            }
+        });
+    }
+
     return (
         <>
             <h1>Hello from react!</h1>
+            <TextInput
+                label="Test Input"
+                id="test-input"
+                value={state.form_values["test-input"]}
+                handleChange={handleChange}
+            />
         </>
     )
 }
