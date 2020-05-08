369cbbfd2450dd02e8910b9d5f27a55c6fdecaef
Tom Nicolosi <tom@teeehn.com>
2020-05-08 20:04:45 +0000

    Converted to a React hello  world.

--- Index.js
+++
@@ -1,8 +0,0 @@
-// This is a starter file.
-(function(){
-    function init() {
-        const root = document.getElementById("root");
-        root.innerHTML = "<h1>Hello gitpod!</h1>";
-    }
-    window.onload = init;
-}());
---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <>
+            <h1>Hello from react!</h1>
+        </>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
